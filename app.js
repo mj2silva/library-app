ba55ede8cfb23ab6ba50aae042c864b8fe7eb3fc
Manuel Silva <manuel.silvag1@gmail.com>
2020-05-17 01:01:51 -0500

    Serving index.html static file

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.send('Hello from this side of the world');
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(3000, () => {
