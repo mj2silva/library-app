01f6a051e7a9cfa25776ae5b44d3d72cf80e445e
Manuel Silva <manuel.silvag1@gmail.com>
2020-05-17 02:26:29 -0500

    add eslint configuration

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/materialize-css/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/materialize-css/dist/js')))
+app.use('/css', express.static(path.join(__dirname, '/node_modules/materialize-css/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/materialize-css/dist/js')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
