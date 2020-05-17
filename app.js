26159b85dab0dc99437164380f48169ffdb29e68
Manuel Silva <manuel.silvag1@gmail.com>
2020-05-17 01:17:51 -0500

    add serving materialize-css through static files

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const path = require('path');
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/materialize-css/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/materialize-css/dist/js')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
