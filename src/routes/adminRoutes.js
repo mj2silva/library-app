351f57820ed15b3f96ef99ba732e1e570e52407c
Manuel Silva <manuel.silvag1@gmail.com>
2020-05-22 04:40:17 -0500

    Added conection with mongo and better styling
    Color palette defined, conection with mongo in localhost
    Improved styles and sign-up form

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('app:adminRoutes');
 
-const router = (header) => {
+const books = require('../data/books');
+
+const router = (/* header */) => {
   const adminRouter = express.Router();
 
   adminRouter.route('/')
     .get((req, res) => {
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+      const mongo = async () => {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to the server');
+          const db = client.db(dbName);
+          const response = await db.collection('books').insertMany(books);
+          res.json(response);
+        } catch (error) {
+          debug(error.stack);
+        }
+        client.close();
+      };
+      mongo();
       res.send('inserting books');
     });
 
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,11 +1,58 @@
 const express = require('express');
-const books = require('../data/books');
+const { MongoClient, ObjectId } = require('mongodb');
+const debug = require('debug')('app:bookRoutes');
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'libraryApp';
+
+const getBooksCollection = async (client) => {
+  let collection;
+  try {
+    debug('Connected correctly to the server');
+    const db = client.db(dbName);
+    collection = await db.collection('books');
+  } catch (error) {
+    collection = error.stack;
+  }
+  return collection;
+};
+
+const getBooks = async (client) => {
+  let books;
+  try {
+    const collection = await getBooksCollection(client);
+    books = await collection.find().toArray();
+  } catch (error) {
+    books = error.stack;
+    debug(books);
+  } finally {
+    client.close();
+  }
+  return books;
+};
+
+const getBook = async (client, bookId) => {
+  let book;
+  try {
+    const collection = await getBooksCollection(client);
+    const id = new ObjectId(bookId);
+    book = await collection.findOne({ _id: id });
+  } catch (error) {
+    book = error.stack;
+    debug(book);
+  } finally {
+    client.close();
+  }
+  return book;
+};
 
 const router = (header) => {
   const bookRouter = express.Router();
 
   bookRouter.route('/')
-    .get((req, res) => {
+    .get(async (req, res) => {
+      const client = await MongoClient.connect(url);
+      const books = await getBooks(client);
       res.render('books', {
         ...header,
         books,
@@ -13,11 +60,13 @@ const router = (header) => {
     });
 
   bookRouter.route('/:id')
-    .get((req, res) => {
+    .get(async (req, res) => {
       const { id } = req.params;
+      const client = await MongoClient.connect(url);
+      const book = await getBook(client, id);
       res.render('singleBook', {
         ...header,
-        book: books[id],
+        book,
       });
     });
 
